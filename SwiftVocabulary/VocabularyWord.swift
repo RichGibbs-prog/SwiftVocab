b815807e8b873dc5866f2f85f3ddc1902cddf14d
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-11 15:41:25 -0700

    First commit

---
+++ SwiftVocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  SwiftVocabulary
+//
+//  Created by Richard Gibbs on 5/11/20.
+//  Copyright © 2020 Rich Gibbs. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+  var word: String
+  var definition: String
+}
